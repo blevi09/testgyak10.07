495448d830169fdf9825f8e23753684eedcd0217
blevi09 <leventeboda9@gmail.com>
2021-10-07 11:06:15 +0200

    test for putIn

--- test.test.js
+++ test.test.js
@@ -1,15 +1,18 @@
 import {ATM} from './ATM'
 
 describe('test for ATM class', () => {
-    describe('test for the constructor and withdraw methods', () => {
+    describe('test for the putIn method', () => {
         let myATM;
         beforeEach(() =>{
             myATM = new ATM(3000);
         });
 
-        test ('constructor work', () => {
-            expect(myATM).toBeInstanceOf(ATM);
-            expect(myATM.getBalance()).toBe(3000);
+        test ('balance should be 3000', () => {
+          expect(myATM.getBalance()).toBe(3000);
         });
+
+        /*test ('constructor work', () => {
+            
+        });*/
     });
 });
